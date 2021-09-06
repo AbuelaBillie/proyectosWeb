92e5a131f5299755ed5dd4e1a1f411e8313d6245
AbuelaBillie <ncguede@gmail.com>
2021-09-05 23:09:53 -0300

    solucion de direcciones relativas y utilidad del formulario contacto

--- fracMat/mails.php
+++ fracMat/mails.php
@@ -9,17 +9,16 @@ $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
-$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message = "Este mensaje fue enviado por " . $name . " \r\n";
 $message .= "Su e-mail es: " . $mail . " \r\n";
 $message .= "Teléfono de contacto: " . $phone . " \r\n";
-$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
 $message .= "Enviado el: " . date('d/m/Y', time());
 
 $para = 'ncguede@gmail.com';
-$asunto = 'Mensaje de' . $name;
+$asunto = 'Mensaje de ' . $name;
 
 mail($para, $asunto, utf8_decode($message), $header);
 
 header("Location:index.html");
 ?>
--->
