692959ef3f1e1072ac62c5662eeb0ecf24bfe167
AbuelaBillie <ncguede@gmail.com>
2021-10-21 02:48:53 -0300

    Subo proyecto mejorPsi

---
+++ mejorPsi/scripts/generadorServicios.js
@@ -0,0 +1,17 @@
+const servicio = document.querySelector(".contenedorServicios");
+const renderizarServicios = () => {
+  servicios.forEach((obj) => {
+    const cardHTML = 
+        `<div class="servicio">
+          <img src="./img/iconMejorPsi.png" alt="Icono Mejor.psi">
+        <div class="nombreServicio">
+            <h3>${obj.servicio}</h3>
+          </div>
+          <div class="comentarioServicio">
+            <p>${obj.descripcion}</p>
+          </div>
+          </div>`;
+    servicio.innerHTML += cardHTML;
+    });
+};
+renderizarServicios();
